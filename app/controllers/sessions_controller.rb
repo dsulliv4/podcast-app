6a0156a5c6489aa0de089e2c7f4754d082b235e0
dsulliv4 <dsulliv4@binghamton.edu>
2021-01-01 01:26:42 -0500

    updated controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,26 @@ class SessionsController < ApplicationController
 def welcome
 end
 
+def destroy
+    session[:user_id].destroy
+    redirect_to '/'
+end 
 
+
+def new
+end
+
+def create
+    #try to find the user in the system
+    User.find_by(username: params[:user][:username])
+    #did we find someone and did they put in the password?
+    if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+       redirect_to user_path(@user)
+    else 
+        flash[:error] = "Sorry, your login info is incorrect. Please try again."
+        redirect_to login_path
+     end
+    end 
 end 
 
