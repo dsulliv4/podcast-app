6a90a0ad415763f4b38408c2f8408654c36ed2e0
dsulliv4 <dsulliv4@binghamton.edu>
2021-01-10 04:10:16 -0500

    categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,20 @@
 class CategoriesController < ApplicationController
-    
+    before_action :get_category, only: [:show]
+   
+   
+    def index
+        @categories = Category.all
+    end 
+
+   def show
+       @podcasts = @category.podcasts.all
+    end
+
+
+    private 
+
+
+        def get_category
+            @category = Category.find(params[:id])
+        end
 end
