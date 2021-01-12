2a5eeba8ca7e208556444e7be331003069f4a50d
dsulliv4 <dsulliv4@binghamton.edu>
2021-01-12 01:21:41 -0500

    README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1> PodLife </h1>
+<br>
+<h2> A Flatiron School Ruby on Rails API </h2>
+<br>
+<h3> Introduction </h3>
+<h4> Ruby on Rails project designed for users to document the podcasts they have listened to and explore others that may pique their interest. </h4>
+<br>
+<h3> Installation </h3>
+<br>
+<h4> 1. Fork and clone repo to your local machine.
+    2. Use 'bundle install' to install dependencies.
+</h4>
+<br>
+<h3>
+    1. Run rails db:migrate.
+    2. Run rails s. Then you can go to your browser and view the application.
+</h3>
+<br>
+
+
+<h4>License</h4>
+<h3>
+The project is available as open source under the terms of the MIT License.</h3>
--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,46 +1,47 @@
 class PodcastsController < ApplicationController
 
-    def new
-      @podcast = Podcast.new
-    end
-  
-    def create
-        if params[:podcast][:id].present?  
-          @podcast = Podcast.find_by(id: params[:podcast][:id]) 
-        else
-          @podcast = Podcast.find_or_create_by(podcast_params) # if it is not present, then they want to create a new one, use find or create so it doesn't make duplicates
-        end
-    
-        redirect_to podcast_path(@podcast)
+  def new
+    @podcast = Podcast.new
+  end
+
+  def create
+      if params[:podcast][:id].present?
+        @podcast = Podcast.find_by(id: params[:podcast][:id])
+      else
+        @podcast = Podcast.find_or_create_by(podcast_params) # if it is not present, then they want to create a new one, use find or create so it doesn't make duplicates
+        UsersPodcast.create!(user: current_user, podcast: @podcast)
       end
-  
-  
-    def show
-    
-      @podcast = Podcast.find(params[:id])
 
+      redirect_to podcast_path(@podcast)
     end
 
-    def most_recent
-        @podcasts = Podcast.most_recent
-    end
-  
-    def index
-      @podcasts = Podcast.all
-    end
-  
-  private
-  
-  def podcast_params
-    params.require(:podcast).permit(:name, :author, :description, :category_id)
+
+  def show
+
+    @podcast = Podcast.find(params[:id])
+
   end
-  
-    # def logged_in_user
-    #   unless logged_in?
-    #     store_location
-    #     flash[:danger] = "Please log in."
-    #     redirect_to login_url
-    #   end
-    # end
-  
+
+  def most_recent
+      @podcasts = Podcast.most_recent
   end
+
+  def index
+    @podcasts = Podcast.all
+  end
+
+private
+
+def podcast_params
+  params.require(:podcast).permit(:name, :author, :description, :category_id)
+end
+
+  # def logged_in_user
+  #   unless logged_in?
+  #     store_location
+  #     flash[:danger] = "Please log in."
+  #     redirect_to login_url
+  #   end
+  # end
+
+end
